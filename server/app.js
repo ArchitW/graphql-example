5f3cd71b9792c32696ab1491fa256d6acbd0bcd4
Archit Warghane <archit2x3@hotmail.com>
2020-02-03 15:06:44 +0530

    mongoose

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require('./schema/schema');
+const mongoose = require("mongoose");
 const app = express();
 
-//middleware
+//Db
+mongoose.connect('mongodb://gq-user:Test1234@ds215219.mlab.com:15219/gql-example',{ useNewUrlParser: true });
+mongoose.connection.once('open',() => {
+  console.log('connected to DB');
+});
 
+//middleware
 app.use('/graphql',graphqlHTTP ({
 //  schema:schema
   schema,
