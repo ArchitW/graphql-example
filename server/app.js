081d339d9f2089652f15c87a13690015d95d6f82
Archit Warghane <archit2x3@hotmail.com>
2020-02-04 19:45:00 +0530

    working GetBooks Query

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require('./schema/schema');
 const mongoose = require("mongoose");
-const app = express();
+const cors = require('cors');
 
+const app = express();
+//allow cross origin requests
+app.use(cors());
 //Db
 mongoose.connect('mongodb://gq-user:Test1234@ds215219.mlab.com:15219/gql-example', {useNewUrlParser: true, useUnifiedTopology:true});
 mongoose.connection.once('open', () => {
