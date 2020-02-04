d1743f2fd1c241dd06ce77e4e79ec054c57d2b47
Archit Warghane <archit2x3@hotmail.com>
2020-02-04 21:05:34 +0530

    setting state with functional component

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,9 +1,17 @@
-import React ,{Component} from 'react';
+import React ,{Component, useState} from 'react';
 import {gql} from 'apollo-boost';
 import {useQuery} from "@apollo/react-hooks";
 import {getBooksQuery,getAuthorsQuery} from "../queries/queries";
 
+
 const AddBook = () =>{
+//https://dev.to/zeyadetman/from-reactjs-class-component-to-functional-component---usestate-part-1-2l3m
+// https://codesandbox.io/s/kkm2o57ky3
+
+    const [name,setName ] = useState('');
+    const [genre,setGenre ] = useState('');
+    const [authorId,setAuthorId ] = useState('');
+
     const {loading, error, data } = useQuery(getAuthorsQuery);
     if(loading) return <p>Loading</p>
     if(error) return <p>Error</p>
@@ -12,20 +20,31 @@ const AddBook = () =>{
         return <option value={author.id} key={author.id}>{author.name}</option>
     });
 
+    const handleSubmitForm = (e) =>{
+        e.preventDefault();
+        console.log(name , genre, authorId);
+
+    }
     return(
-        <form action="">
+        <form onSubmit={handleSubmitForm}>
             <div className="field">
                 <label htmlFor="">Book Name</label>
-                <input type="text"/>
+                <input type="text"
+                       placeholder={name}
+                       onChange={(e) => setName(e.target.value)}/>
             </div>
             <div className="field">
                 <label htmlFor="">Gener</label>
-                <input type="text"/>
+                <input type="text"
+                placeholder={genre}
+                onChange={(e)=>setGenre(e.target.value)}/>
             </div>
             <div className="field">
                 <label htmlFor="">Author</label>
-                <select>
-                    <option value="">Select Value</option>
+                <select
+                value={authorId}
+                onChange={(e) => setAuthorId(e.target.value)}>
+
                     {AuthorsList}
                 </select>
             </div>
